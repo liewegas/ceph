4f2f9234d48fac0a917a6ba0b2d49d8f017ef95c
Sage Weil <sage@inktank.com>
2012-07-14 07:57:23 -0700

    msg/Pipe: move tcp_* functions into Pipe class
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/msg/Pipe.cc
+++ src/msg/Pipe.cc
@@ -16,12 +16,14 @@
 #include <netinet/tcp.h>
 #include <sys/uio.h>
 #include <limits.h>
+#include <poll.h>
 
 #include "Message.h"
 #include "Pipe.h"
 #include "SimpleMessenger.h"
 
 #include "common/debug.h"
+#include "common/errno.h"
 
 #define dout_subsys ceph_subsys_ms
 
@@ -1763,20 +1765,119 @@ int Pipe::write_message(Message *m)
 
 int Pipe::tcp_read(char *buf, int len)
 {
-  return ::tcp_read(msgr->cct, sd, buf, len, msgr->timeout);
+  if (sd < 0)
+    return -1;
+
+  while (len > 0) {
+
+    if (msgr->cct->_conf->ms_inject_socket_failures && sd >= 0) {
+      if (rand() % msgr->cct->_conf->ms_inject_socket_failures == 0) {
+	ldout(msgr->cct, 0) << "injecting socket failure" << dendl;
+	::shutdown(sd, SHUT_RDWR);
+      }
+    }
+
+    if (tcp_read_wait() < 0)
+      return -1;
+
+    int got = tcp_read_nonblocking(buf, len);
+
+    if (got < 0)
+      return -1;
+
+    len -= got;
+    buf += got;
+    //lgeneric_dout(cct, DBL) << "tcp_read got " << got << ", " << len << " left" << dendl;
+  }
+  return len;
 }
 
 int Pipe::tcp_read_wait()
 {
-  return ::tcp_read_wait(sd, msgr->timeout);
+  if (sd < 0)
+    return -1;
+  struct pollfd pfd;
+  short evmask;
+  pfd.fd = sd;
+  pfd.events = POLLIN;
+#if defined(__linux__)
+  pfd.events |= POLLRDHUP;
+#endif
+
+  if (poll(&pfd, 1, msgr->timeout) <= 0)
+    return -1;
+
+  evmask = POLLERR | POLLHUP | POLLNVAL;
+#if defined(__linux__)
+  evmask |= POLLRDHUP;
+#endif
+  if (pfd.revents & evmask)
+    return -1;
+
+  if (!(pfd.revents & POLLIN))
+    return -1;
+
+  return 0;
 }
 
 int Pipe::tcp_read_nonblocking(char *buf, int len)
 {
-  return ::tcp_read_nonblocking(msgr->cct, sd, buf, len);
+again:
+  int got = ::recv( sd, buf, len, MSG_DONTWAIT );
+  if (got < 0) {
+    if (errno == EAGAIN || errno == EINTR) {
+      goto again;
+    } else {
+      ldout(msgr->cct, 10) << "tcp_read_nonblocking socket " << sd << " returned "
+		     << got << " errno " << errno << " " << cpp_strerror(errno) << dendl;
+      return -1;
+    }
+  } else if (got == 0) {
+    /* poll() said there was data, but we didn't read any - peer
+     * sent a FIN.  Maybe POLLRDHUP signals this, but this is
+     * standard socket behavior as documented by Stevens.
+     */
+    return -1;
+  }
+  return got;
 }
 
 int Pipe::tcp_write(const char *buf, int len)
 {
-  return ::tcp_write(msgr->cct, sd, buf, len);
+  if (sd < 0)
+    return -1;
+  struct pollfd pfd;
+  pfd.fd = sd;
+  pfd.events = POLLOUT | POLLHUP | POLLNVAL | POLLERR;
+#if defined(__linux__)
+  pfd.events |= POLLRDHUP;
+#endif
+
+  if (msgr->cct->_conf->ms_inject_socket_failures && sd >= 0) {
+    if (rand() % msgr->cct->_conf->ms_inject_socket_failures == 0) {
+      ldout(msgr->cct, 0) << "injecting socket failure" << dendl;
+      ::shutdown(sd, SHUT_RDWR);
+    }
+  }
+
+  if (poll(&pfd, 1, -1) < 0)
+    return -1;
+
+  if (!(pfd.revents & POLLOUT))
+    return -1;
+
+  //lgeneric_dout(cct, DBL) << "tcp_write writing " << len << dendl;
+  assert(len > 0);
+  while (len > 0) {
+    int did = ::send( sd, buf, len, MSG_NOSIGNAL );
+    if (did < 0) {
+      //lgeneric_dout(cct, 1) << "tcp_write error did = " << did << "  errno " << errno << " " << strerror(errno) << dendl;
+      //lgeneric_derr(cct, 1) << "tcp_write error did = " << did << "  errno " << errno << " " << strerror(errno) << dendl;
+      return did;
+    }
+    len -= did;
+    buf += did;
+    //lgeneric_dout(cct, DBL) << "tcp_write did " << did << ", " << len << " left" << dendl;
+  }
+  return 0;
 }
