ef0af0033f19595038d74ed40636740dfa9b4796
Sage Weil <sage@redhat.com>
2015-09-23 10:25:30 -0400

    osd: do full check in do_op
    1. The current pool_last_map_marked_full tracking is buggy.
    2. We need to recheck this each time we consider the op, not just when it
    is received off the wire.  Otherwise, we might get a message, queue it
    for some reason, get a map indicating the cluster or pool is full, and
    then requeue and process the op instead of discarding it.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -1496,6 +1496,22 @@ void ReplicatedPG::do_op(OpRequestRef& op)
     return;
   }
 
+  // discard due to cluster full transition?  (we discard any op that
+  // originates before the cluster or pool is marked full; the client
+  // will resend after the full flag is removed or if they expect the
+  // op to succeed despite being full).
+  // FIXME: we exclude mds writes for now.
+  if (!m->get_source().is_mds() &&
+      info.history.last_epoch_marked_full > m->get_map_epoch()) {
+    dout(10) << __func__ << " discarding op sent before full " << m << " "
+	     << *m << dendl;
+    return;
+  }
+  if (osd->check_failsafe_full()) {
+    osd->reply_op_error(op, -ENOSPC);
+    return;
+  }
+
   // order this op as a write?
   bool write_ordered =
     op->may_write() ||
