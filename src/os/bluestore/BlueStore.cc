e369d8515ff593ca234133ab7c3ec64e953259ef
Sage Weil <sage@redhat.com>
2016-05-19 16:29:19 -0400

    os/bluestore: _dump_onode crcs in hex
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -5079,7 +5079,8 @@ void BlueStore::_dump_onode(OnodeRef o, int log_level)
       unsigned n = b.second.get_csum_count();
       for (unsigned i = 0; i < n; ++i)
 	v.push_back(b.second.get_csum_item(i));
-      dout(log_level) << __func__ << "       csum: " << v << dendl;
+      dout(log_level) << __func__ << "       csum: " << std::hex << v << std::dec
+		      << dendl;
     }
   }
   pos = 0;
@@ -5123,7 +5124,8 @@ void BlueStore::_dump_bnode(BnodeRef b, int log_level)
       unsigned n = p.second.get_csum_count();
       for (unsigned i = 0; i < n; ++i)
 	v.push_back(p.second.get_csum_item(i));
-      dout(log_level) << __func__ << "       csum: " << v << dendl;
+      dout(log_level) << __func__ << "       csum: " << std::hex << v << std::dec
+		      << dendl;
     }
   }
 }
