fb1a98402e1b64e01bb85d4534944623ea6c2e8f
Victor Araujo <ve.ar91@gmail.com>
2016-09-05 00:48:33 +0200

    blkin: Pass trace information for rbd_aio_write and rbd_aio_read
    New rbd_aio_write_traced and rbd_aio_read_traced are provided in librbd.
    These take a blkin_trace_info* struct that holds the identifiers for
    Zipkin tracing. Trace objects are constructed and passed along to librados
    aio_operate overloads that take trace information.
    
    Signed-off-by: Victor Araujo <ve.ar91@gmail.com>

--- src/librbd/AioObjectRequest.h
+++ src/librbd/AioObjectRequest.h
@@ -9,6 +9,7 @@
 #include <map>
 
 #include "common/snap_types.h"
+#include "common/zipkin_trace.h"
 #include "include/buffer.h"
 #include "include/rados/librados.hpp"
 #include "librbd/ObjectMap.h"
@@ -69,7 +70,8 @@ public:
   AioObjectRequest(ImageCtx *ictx, const std::string &oid,
                    uint64_t objectno, uint64_t off, uint64_t len,
                    librados::snap_t snap_id,
-                   Context *completion, bool hide_enoent);
+                   Context *completion, bool hide_enoent,
+                   ZTracer::Trace *trace = nullptr);
   virtual ~AioObjectRequest() {}
 
   virtual void add_copyup_ops(librados::ObjectWriteOperation *wr) {};
@@ -100,6 +102,7 @@ protected:
   Context *m_completion;
   Extents m_parent_extents;
   bool m_hide_enoent;
+  ZTracer::Trace m_trace;
 
 private:
   bool m_has_parent = false;
@@ -195,7 +198,8 @@ public:
   AbstractAioObjectWrite(ImageCtx *ictx, const std::string &oid,
                          uint64_t object_no, uint64_t object_off,
                          uint64_t len, const ::SnapContext &snapc,
-                         Context *completion, bool hide_enoent);
+                         Context *completion, bool hide_enoent,
+                         ZTracer::Trace *trace = nullptr);
 
   virtual void add_copyup_ops(librados::ObjectWriteOperation *wr)
   {
@@ -279,9 +283,9 @@ public:
   AioObjectWrite(ImageCtx *ictx, const std::string &oid, uint64_t object_no,
                  uint64_t object_off, const ceph::bufferlist &data,
                  const ::SnapContext &snapc, Context *completion,
-                 int op_flags)
+                 int op_flags, ZTracer::Trace *trace = nullptr)
     : AbstractAioObjectWrite(ictx, oid, object_no, object_off, data.length(),
-                             snapc, completion, false),
+                             snapc, completion, false, trace),
       m_write_data(data), m_op_flags(op_flags) {
   }
 
