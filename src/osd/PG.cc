aa5514e8ec13098c90918a89b6ff882a6e4bb8c6
Sage Weil <sage@redhat.com>
2016-11-22 21:52:38 -0600

    osd/PG: do not trim past_intervals that may still be readable
    We need to maintain information about past intervals long enough to
    decide whether to wait for them to become unreadable.  Do not trim them
    if they match or follow pg_history_t::oldest_interval_readable.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -875,7 +875,8 @@ void PG::generate_past_intervals()
 /*
  * Trim past_intervals.
  *
- * This gets rid of all the past_intervals that happened before last_epoch_clean.
+ * This gets rid of all the past_intervals that happened before
+ * last_epoch_clean AND oldest_interval_readable
  */
 void PG::trim_past_intervals()
 {
@@ -883,6 +884,8 @@ void PG::trim_past_intervals()
   std::map<epoch_t,pg_interval_t>::iterator end = past_intervals.end();
   while (pif != end) {
     if (pif->second.last >= info.history.last_epoch_clean)
+      return;
+    if (pif->second.last >= info.history.oldest_interval_readable)
       return;
     dout(10) << __func__ << ": trimming " << pif->second << dendl;
     past_intervals.erase(pif++);
