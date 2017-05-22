ca695073087ee5a855eeea75770c605a48835fc9
Sage Weil <sage@redhat.com>
2017-05-21 22:56:34 -0400

    osd: work around bluestore fragmetned buffers in get_map_bl
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -1399,8 +1399,14 @@ bool OSDService::_get_map_bl(epoch_t e, bufferlist& bl)
     return true;
   found = store->read(coll_t::meta(),
 		      OSD::get_osdmap_pobject_name(e), 0, 0, bl) >= 0;
-  if (found)
+  if (found) {
+    if (bl.get_num_buffers() > 1) {
+      // bluestore may return a fragmented buffer, which will make the denc
+      // decodes slow.
+      bl.rebuild();
+    }
     _add_map_bl(e, bl);
+  }
   return found;
 }
 
@@ -1412,8 +1418,14 @@ bool OSDService::get_inc_map_bl(epoch_t e, bufferlist& bl)
     return true;
   found = store->read(coll_t::meta(),
 		      OSD::get_inc_osdmap_pobject_name(e), 0, 0, bl) >= 0;
-  if (found)
+  if (found) {
+    if (bl.get_num_buffers() > 1) {
+      // bluestore may return a fragmented buffer, which will make the denc
+      // decodes slow.
+      bl.rebuild();
+    }
     _add_map_inc_bl(e, bl);
+  }
   return found;
 }
 
