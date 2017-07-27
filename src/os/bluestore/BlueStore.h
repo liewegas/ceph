48371ab5a200db56ab3231f8dc2f85c4d289ac5f
Neha Ojha <nojha@redhat.com>
2017-07-27 13:13:13 -0700

    osd: reimplement is_journal_rotational for bluestore
    Signed-off-by: Neha Ojha <nojha@redhat.com>

--- src/os/bluestore/BlueStore.h
+++ src/os/bluestore/BlueStore.h
@@ -1980,7 +1980,6 @@ private:
 				   bool create);
 
   int _write_bdev_label(string path, bluestore_bdev_label_t label);
-  bool _is_bdev_journal_rotational();
 public:
   static int _read_bdev_label(CephContext* cct, string path,
 			      bluestore_bdev_label_t *label);
