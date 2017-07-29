6cf5c1cbfa8a9ee7c4685a88170f59e844a81aab
Spandan Kumar Sahu <spandankumarsahu@gmail.com>
2017-07-29 14:53:19 +0530

    src/pybind/mgr/balancer/module.py: implement 'do_osd_weight'
    * make cost_model a config
    * use existing reweight-by-utilization and reweight-by-pg
    
    Signed-off-by: Spandan Kumar Sahu <spandankumarsahu@gmail.com>

--- src/pybind/mgr/balancer/module.py
+++ src/pybind/mgr/balancer/module.py
@@ -13,11 +13,11 @@ from threading import Event
 
 # available modes: 'none', 'crush', 'crush-compat', 'upmap', 'osd_weight'
 default_mode = 'none'
+default_cost_mode = 'pg'
 default_sleep_interval = 60   # seconds
 default_max_misplaced = .03   # max ratio of pgs replaced at a time
 
 TIME_FORMAT = '%Y-%m-%d_%H:%M:%S'
-
 
 class MappingState:
     def __init__(self, osdmap, pg_dump, desc=''):
@@ -172,8 +172,13 @@ class Module(MgrModule):
             "perm": "r",
         },
         {
-            "cmd": "balancer mode name=mode,type=CephChoices,strings=none|crush-compat|upmap",
+            "cmd": "balancer mode name=mode,type=CephChoices,strings=none|crush-compat|upmap|osd_weight",
             "desc": "Set balancer mode",
+            "perm": "rw",
+        },
+        {
+            "cmd": "balancer cost-mode name=cost-mode,type=CephChoices,strings=pg|pg-size|utilization",
+            "desc": "Set balancer cost mode",
             "perm": "rw",
         },
         {
@@ -238,10 +243,14 @@ class Module(MgrModule):
                 'plans': self.plans.keys(),
                 'active': self.active,
                 'mode': self.get_config('mode', default_mode),
+                'cost-mode': self.get_config('cost_mode', default_cost_mode),
             }
             return (0, json.dumps(s, indent=4), '')
         elif command['prefix'] == 'balancer mode':
             self.set_config('mode', command['mode'])
+            return (0, '', '')
+        elif command['prefix'] == 'balancer cost-mode':
+            self.set_config('cost_mode', command['cost-mode'])
             return (0, '', '')
         elif command['prefix'] == 'balancer on':
             if not self.active:
@@ -541,6 +550,8 @@ class Module(MgrModule):
                 return self.do_upmap(plan)
             elif plan.mode == 'crush-compat':
                 return self.do_crush_compat(plan)
+            elif plan.mode == 'osd_weight':
+                self.do_osd_weight()
             elif plan.mode == 'none':
                 self.log.info('Idle')
             else:
@@ -602,6 +613,11 @@ class Module(MgrModule):
             return False
 
         key = 'pgs'  # pgs objects or bytes
+        # select a cost mode: pg, pgsize, or utilization
+        # FIXME: when we add utilization support, we need to throttle back
+        # so that we don't run if *any* objects are misplaced.  with 'pgs' we
+        # can look at up mappings, which lets us look ahead a bit.
+        cost_mode = self.get_config('cost_mode', default_cost_mode)
 
         # go
         random.shuffle(roots)
@@ -733,7 +749,29 @@ class Module(MgrModule):
         self.log.info('do_crush (not yet implemented)')
 
     def do_osd_weight(self):
-        self.log.info('do_osd_weight (not yet implemented)')
+        self.log.info('do_osd_weight')
+
+        cost_mode = self.get_config('cost_mode', default_cost_mode)
+
+	# Reweight any over-utilized OSD
+        result = CommandResult('balancer_osd_weight')
+        if cost_mode == 'pg':
+            self.send_command(result, 'mon', '', json.dumps({
+                'prefix': 'osd reweight-by-pg',
+                'oload': '101',
+            }), 'balancer_osd_weight')
+            r, outb, outs = result.wait()
+        elif cost_mode == 'utilization':
+            self.send_command(result, 'mon', '', json.dumps({
+                'prefix': 'osd reweight-by-utilization',
+                'oload': '101',
+            }), 'balancer_osd_weight')
+            r, outb, outs = result.wait()
+        else:
+            raise RuntimeError('unsupported cost mode %s' % cost_mode)
+        if r != 0:
+            self.log.error(outs)
+            return
 
     def execute(self, plan):
         self.log.info('Executing plan %s' % plan.name)
@@ -814,10 +852,6 @@ class Module(MgrModule):
                 'pgid': item['pgid'],
                 'id': osdlist,
             }), 'foo')
-            commands.append(result)
-
-        # wait for commands
-        for result in commands:
             r, outb, outs = result.wait()
             if r != 0:
                 self.log.error('Error on command')
