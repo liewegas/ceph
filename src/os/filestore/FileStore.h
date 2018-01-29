a15a93b612657c16eaa08028652920618442cdcc
Sage Weil <sage@redhat.com>
2018-01-29 17:26:33 -0600

    os/filestore: more efficient waiter tracking
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/os/filestore/FileStore.h
+++ src/os/filestore/FileStore.h
@@ -230,7 +230,8 @@ private:
     list<uint64_t> jq;
     list<pair<uint64_t, Context*> > flush_commit_waiters;
     Cond cond;
-    map<ghobject_t,int> applying;
+    /// hash of pointers to ghobject_t's for in-flight writes
+    unordered_multimap<uint32_t,const ghobject_t*> applying;
   public:
     Mutex apply_lock;  // for apply mutual exclusion
     int id;
