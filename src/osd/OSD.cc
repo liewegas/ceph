60b89067c1c501682d96188b9416b01fd66b781e
Sage Weil <sage@redhat.com>
2018-03-06 09:37:46 -0600

    osd: share store_stat_t for each pool with mgr
    Signed-off-by: Igor Fedotov <ifedotov@suse.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -7020,18 +7020,34 @@ MPGStats* OSD::collect_pg_stats()
   Mutex::Locker lec{min_last_epoch_clean_lock};
   min_last_epoch_clean = osdmap->get_epoch();
   min_last_epoch_clean_pgs.clear();
-  RWLock::RLocker lpg(pg_map_lock);
-  for (const auto &i : pg_map) {
-    PG *pg = i.second;
-    if (!pg->is_primary()) {
-      continue;
-    }
+  std::set<int64_t> pool_set;
+  {
+    RWLock::RLocker lpg(pg_map_lock);
+    for (const auto &i : pg_map) {
+      PG *pg = i.second;
+      auto pool = pg->pg_id.pgid.pool();
+      pool_set.emplace((int64_t)pool);
+      if (!pg->is_primary()) {
+	continue;
+      }
+      pg->get_pg_stats([&](const pg_stat_t& s, epoch_t lec) {
+	  m->pg_stat[pg->pg_id.pgid] = s;
 
-    pg->get_pg_stats([&](const pg_stat_t& s, epoch_t lec) {
-	m->pg_stat[pg->pg_id.pgid] = s;
-	min_last_epoch_clean = min(min_last_epoch_clean, lec);
-	min_last_epoch_clean_pgs.push_back(pg->pg_id.pgid);
-      });
+	  min_last_epoch_clean = min(min_last_epoch_clean, lec);
+	  min_last_epoch_clean_pgs.push_back(pg->pg_id.pgid);
+	});
+    }
+  }
+
+  store_statfs_t st;
+  for (auto p : pool_set) {
+    int r = store->pool_statfs(p, &st);
+    if (r == -ENOTSUP) {
+      break;
+    } else {
+      assert(r >= 0);
+      m->pool_stat[p] = st;
+    }
   }
 
   return m;
